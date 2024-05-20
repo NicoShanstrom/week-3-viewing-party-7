a438730e5a2580fdfa0545929f7f2c2b926d5542
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-20 17:05:06 -0400

    Add sessions to create and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController <ApplicationController
   def create 
     user = User.create(user_params)
     if user.save
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user)
     else  
@@ -28,6 +29,7 @@ class UsersController <ApplicationController
   def login
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(user)
     else
