cb2160fdfca2497dcb7c68f69c39cf4cc24542d9
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-20 21:42:34 -0400

    Application controller modifications and add comment for learning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @logged_in_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
+# so if @logged_in_user is nil, then it goes to user.find... IF a user_id session has initiated,
+# otherwise the whole expression is nil
+
+# The ||= operator is a shorthand for memoization. 
+  # It means "assign the value to the variable if the variable is currently nil or false."
+# The expression User.find(session[:user_id]) is only executed if @session_user is nil or undefined.
+# The if session[:user_id] part ensures that User.find(session[:user_id]) is only called if session[:user_id] is present
+#  If session[:user_id] is nil, the whole expression returns nil.
