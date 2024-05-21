e05a243abfa11a777593fd8c30b765e883fbb6c6
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-20 21:16:14 -0400

    Modify user create action for .downcase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController <ApplicationController 
 
   def new 
-    @user = User.new()
+    @user = User.new
   end 
 
   def show 
@@ -9,13 +9,15 @@ class UsersController <ApplicationController
   end 
 
   def create 
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.name}!"
-      redirect_to user_path(user)
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.create(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      flash[:success] = "Welcome, #{new_user.name}!"
+      redirect_to user_path(new_user)
     else  
-      flash[:error] = user.errors.full_messages.to_sentence
+      flash[:error] = new_user.errors.full_messages.to_sentence
       redirect_to register_path
     end 
   end
