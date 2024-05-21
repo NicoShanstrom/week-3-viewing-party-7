1952935f4b65bc7ea413fc6a082b9421ece7118b
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-21 13:48:10 -0400

    Complete transfer of (login_form) new and (login) create from userscontroller into sessions_controller, refactor routes, deletes login_form view from users, and creates new view in sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,28 +22,7 @@ class UsersController <ApplicationController
     end 
   end
 
-  def login_form
-  end
-
-  def login
-    user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.name}!"
-      # redirect_to user_path(user)
-      if user.admin?
-        redirect_to admin_dashboard_path and return
-      elsif user.manager?
-        redirect_to user_path(user) and return
-      else
-        redirect_to user_path(user) and return
-      end
-    else
-      flash[:error] = "Sorry, your credentials are bad."
-      render :login_form
-    end
-  end
-
+  
   private 
 
   def user_params 
